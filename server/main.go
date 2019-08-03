9017a0ac3a120b8d119d872ce2f475b159c012dc
lge33 <lge33@gatech.edu>
2019-08-03 17:22:04 -0400

    modified naming in proto

--- server/main.go
+++ server/main.go
@@ -3,11 +3,10 @@ package main
 import (
     "context"
     "fmt"
+    "io"
     "log"
     "net"
     "os"
-
-    //"os"
 
     "google.golang.org/grpc"
     pb "../pingpong"
@@ -20,8 +19,8 @@ const (
 // struct to implement the pingpong server
 type server struct{}
 
-// PingPongBackend implements pingpong.PingPongService
-func (s *server) PingPongBackend(ctx context.Context, in *pb.Ping) (*pb.Pong, error) {
+// Backend implements pingpong.PingPong
+func (s *server) Backend(ctx context.Context, in *pb.Ping) (*pb.Pong, error) {
     log.Printf("Received: %v", in.Ping)
     hostname, err := os.Hostname()
     if err != nil {
@@ -30,13 +29,47 @@ func (s *server) PingPongBackend(ctx context.Context, in *pb.Ping) (*pb.Pong, er
     return &pb.Pong{Pong: "Hello " + in.Ping + "! This is " + fmt.Sprint(hostname) + "."}, nil
 }
 
+// PingPongStream implements pingpong.PingPong
+func (s *server) Stream(srv pb.PingPong_StreamServer) error {
+    log.Println("Stream starts")
+    context := srv.Context()
+
+    for {
+        select {
+        case <- context.Done():
+            return context.Err()
+        default:
+
+        }
+
+        req, err := srv.Recv()
+        if err == io.EOF {
+            log.Println("exit")
+            return nil
+        }
+        if err != nil {
+            log.Fatalf("error occurred receiving: %v", err);
+        }
+
+        hostname, err := os.Hostname()
+        if err != nil {
+            log.Fatalf("failed to get hostname: %v", err)
+        }
+        res := pb.Pong{Pong: "Hello " + req.Ping + "! This is " + fmt.Sprint(hostname) + "."}
+        if err := srv.Send(&res); err != nil {
+            log.Fatalf("error occured sending: %v", err)
+        }
+        log.Printf("Sent stream");
+    }
+}
+
 func main() {
     lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", port))
     if err != nil {
         log.Fatalf("failed to listen: %v", err)
     }
     s := grpc.NewServer()
-    pb.RegisterPingPongServiceServer(s, &server{})
+    pb.RegisterPingPongServer(s, &server{})
     log.Printf("Listening on port: %d", port)
     if err := s.Serve(lis); err != nil {
         log.Fatalf("failed to serve: %v", err)
